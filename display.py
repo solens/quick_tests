f6cb52475aa5899998b1571c779b4372e72cc625
solens <ssanschagrin@twm.ch>
2018-09-03 16:09:56 +0200

    1

---
+++ display.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-18 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+
+import re
+import time
+import argparse
+import os
+import sys
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+
+def demo(n, block_orientation, rotate,x):
+    # create matrix device
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation, rotate=rotate or 0)
+    print("Created device")
+
+    device.contrast(16)
+
+    with canvas(device) as draw:
+        text(draw,(0,0),chr(x),fill = "white")
+
+    print("First letters")
+    time.sleep(1)
+    for x in letters_as_num:
+        with canvas(device) as draw:
+            text(draw,(0, 0),chr(x),fill = "white")
+            time.sleep(1)
+
+    print("Symbols")
+    time.sleep(1)
+    for x in sym_as_num:
+        with canvas(device) as draw:
+            text(draw,(0, 0),chr(x),fill = "white")
+            time.sleep(1)
+
+    print("Vent")
+    time.sleep(1)
+    for x in [120,43,120,43]:
+        with canvas(device) as draw:
+            text(draw,(1, 1),chr(x),fill = "white")
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    symbol = sys.args[1]
+    parser = argparse.ArgumentParser(description='matrix_demo arguments',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=90, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+
+    args = parser.parse_args()
+
+    try:
+        demo(args.cascaded, args.block_orientation, args.rotate,symbol)
+    except KeyboardInterrupt:
+        pass
